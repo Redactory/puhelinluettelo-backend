50af4b23d85f2ba8f9a90cbaf1bcb822d7b91d5c
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-09-26 13:56:09 +0300

    Lisää build - kansio ja sen käyttö

---
+++ build/precache-manifest.9b8caafec2b420e546d8d7671edca9a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a877006323b52edc65c200c950fc9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ffcfe9ca540e379f11",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3d312e1861a5e1eebbf4",
+    "url": "/static/js/2.3b75c555.chunk.js"
+  },
+  {
+    "revision": "a9ffcfe9ca540e379f11",
+    "url": "/static/js/main.54c7060a.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
