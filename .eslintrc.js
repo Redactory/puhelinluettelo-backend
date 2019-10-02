da36980678399c944386c30a46cde1c70e536684
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-10-02 13:26:04 +0300

    Tehtävän 3.22 toteutus
    Tässä vaiheessa tuotanto - versio sovelluksesta on päivitetty ja on nähtävillä verkossa.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+  ],
+  "eqeqeq": "error"
+  },
+};
